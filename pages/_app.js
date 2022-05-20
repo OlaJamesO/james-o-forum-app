9887fa7ee96e030152cee5bccd7529af23a6cc10
jokuso01 <okusolubo@gmail.com>
2022-05-20 03:12:08 +0100

    not complete TBC

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import Amplify from "aws-amplify";
-import config from "../src/aws-exports";
 Amplify.configure(config);
+import config from "../src/aws-exports"; 
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
